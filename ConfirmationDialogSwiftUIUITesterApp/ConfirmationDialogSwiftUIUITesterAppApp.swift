f6154977449a570a2641fd01273a27f29682b202
Kavinda Dilshan <kdtechniques@gmail.com>
2024-08-03 20:31:54 +0530

    Initial Commit

---
+++ ConfirmationDialogSwiftUIUITesterApp/ConfirmationDialogSwiftUIUITesterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfirmationDialogSwiftUIUITesterAppApp.swift
+//  ConfirmationDialogSwiftUIUITesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct ConfirmationDialogSwiftUIUITesterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
